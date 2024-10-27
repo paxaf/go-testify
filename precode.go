9135c6a79d83c92f3c3c1040183f23a4a5c9d2ac
Павел Сашков <paul.sashkov@gmail.com>
2024-10-27 22:08:48 +0300

    Use _test.go

--- precode.go
+++ precode.go
@@ -1,15 +1,9 @@
 package main
 
 import (
-	"fmt"
 	"net/http"
-	"net/http/httptest"
 	"strconv"
 	"strings"
-	"testing"
-
-	"github.com/stretchr/testify/assert"
-	"github.com/stretchr/testify/require"
 )
 
 var cafeList = map[string][]string{
@@ -48,43 +42,4 @@ func mainHandle(w http.ResponseWriter, req *http.Request) {
 
 	w.WriteHeader(http.StatusOK)
 	w.Write([]byte(answer))
-}
-
-func TestMainHandlerWhenReqOk(t *testing.T) {
-	req := httptest.NewRequest("GET", "/cafe?count=2&city=moscow", nil)
-
-	responseRecorder := httptest.NewRecorder()
-	handler := http.HandlerFunc(mainHandle)
-	handler.ServeHTTP(responseRecorder, req)
-
-	require.Equal(t, http.StatusOK, responseRecorder.Code, "Status should be OK")
-	body := responseRecorder.Body
-	assert.NotEmpty(t, body, "Body should be not empty")
-}
-
-func TestMainHandlerWhenCountMoreThanTotal(t *testing.T) {
-	totalCount := 4
-	req := httptest.NewRequest("GET", "/cafe?count=10&city=moscow", nil)
-
-	responseRecorder := httptest.NewRecorder()
-	handler := http.HandlerFunc(mainHandle)
-	handler.ServeHTTP(responseRecorder, req)
-
-	require.Equal(t, http.StatusOK, responseRecorder.Code, "Status code should be OK")
-
-	body := responseRecorder.Body.String()
-	list := strings.Split(body, ",")
-	assert.Len(t, list, totalCount, fmt.Sprintf("Total count: %d, should be %d", len(list), totalCount))
-}
-
-func TestMainHandlerUnkownCity(t *testing.T) {
-	req := httptest.NewRequest("GET", "/cafe?count=4&city=berlin", nil)
-
-	responseRecorder := httptest.NewRecorder()
-	handler := http.HandlerFunc(mainHandle)
-	handler.ServeHTTP(responseRecorder, req)
-
-	assert.Equal(t, responseRecorder.Code, http.StatusBadRequest, "Status code should be BadRequest")
-	body := responseRecorder.Body.String()
-	assert.Equal(t, "wrong city value", body, "Body response should be \"wrong city value\" ")
 }
